f9d1ee8dcd3974964e2c0f67e180cae1fda9293c
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-01-05 01:18:05 +0100

    remove the cache

--- src/views/cache/index.6d8a61cb0bdb93ed28ae095a0e486d0e.rtpl.php
+++
@@ -1,3 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><?php require $this->checkTemplate("shared/header/header");?> 
-<?php require $this->checkTemplate("home");?>
-<?php require $this->checkTemplate("shared/footer/footer");?>
--- src/views/cache/index.b6029b79c2a07fc4b6c80d29384585ee.rtpl.php
+++
@@ -1,3 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><?php require $this->checkTemplate("shared/Header/header");?> 
-<?php require $this->checkTemplate("home");?>
-<?php require $this->checkTemplate("shared/Footer/footer");?>
