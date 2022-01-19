2ba36ce965f0f8822fe964a438d95aaaaee80306
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-01-19 01:50:51 +0100

    configure pagination in profile

--- src/controllers/clients/ProductsController.php
+++ src/controllers/clients/ProductsController.php
@@ -3,6 +3,11 @@
 namespace Vendor\controllers\clients;
 
 use Vendor\config\RainTpl;
+
+use Vendor\usecases\GetCategories;
+use Vendor\usecases\GetGlobalProducts;
+use Vendor\usecases\GetGlobalProductsBySearch;
+
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -15,8 +20,42 @@ class ProductsController
     ) {
         $template = new RainTpl();
 
+        $search_input = @$req->getQueryParams()["search"];
+        $page = @$req->getQueryParams()["page"];
+        $category = @$req->getQueryParams()["category"] ?? 0;
+
+        $search = isset($search_input) ? $search_input : "";
+        $currentPage = isset($page) ? (int)$page : 1;
+
+        if ($search !== "") {
+            $paginateBySearch = new GetGlobalProductsBySearch();
+            $pagination = $paginateBySearch->execute($search, $category, $currentPage);
+        } else {
+            $paginate = new GetGlobalProducts();
+            $pagination = $paginate->execute($currentPage);
+        }
+
+        $pages = [];
+        for ($i = 0; $i < $pagination['pages']; $i++) {
+            array_push($pages, [
+                'href' => '/bioloOnline/products?' . http_build_query([
+                    'page' => $i + 1,
+                    'search' => $search,
+                    'category' => $category
+                ]),
+                'text' => $i + 1
+            ]);
+        }
+
+        $categories = new GetCategories();
+        $categoryData = $categories->execute();        
+
         return $template->setTpl("products", [
             "provider" => @$_SESSION["provider"],
+            "categories" => $categoryData,
+            "products" => $pagination['data'],
+            "search" => $search,
+            "pages" => $pages,
         ]);
     }
 }
