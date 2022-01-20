52556da2a8ed4cf26d18af45519b811fa346b1be
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-01-20 13:14:18 +0100

    update purchase interface

--- src/interfaces/IPurchaseRepository.php
+++ src/interfaces/IPurchaseRepository.php
@@ -10,7 +10,7 @@ interface IPurchaseRepository
     public function addProductIntoCart(Purchase $purchase);
     public function showProductIntoCart(array $authenticatedUser);
     public function removeProductIntoCart(int $product_id);
-    public function updateQuantityOfProductIntoCart(Purchase $purchase);
+    public function updateQuantityOfProductFromCart(int $quantity, int $product_id);
 
     public function sendMessageToProvider(ProductProvider $productProvider);
 }
