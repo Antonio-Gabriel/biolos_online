1387b3715f845e659059685d964408e1d662c210
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-01-20 02:17:49 +0100

    add show product cart

--- src/interfaces/IPurchaseRepository.php
+++ src/interfaces/IPurchaseRepository.php
@@ -8,8 +8,9 @@ use Vendor\models\ProductProvider;
 interface IPurchaseRepository
 {
     public function addProductIntoCart(Purchase $purchase);
+    public function showProductIntoCart(array $authenticatedUser);
     public function removeProductIntoCart(int $product_id);
     public function updateQuantityOfProductIntoCart(Purchase $purchase);
-    
+
     public function sendMessageToProvider(ProductProvider $productProvider);
 }
