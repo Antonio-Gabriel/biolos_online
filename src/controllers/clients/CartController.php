025b912135cbd332d8262604af0f440be555b74b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-01-20 11:53:07 +0100

    cart controller

--- src/controllers/clients/CartController.php
+++ src/controllers/clients/CartController.php
@@ -7,23 +7,46 @@ use Vendor\config\RainTpl;
 use Vendor\validators\Middleware;
 use Vendor\usecases\AddProductIntoCart;
 
+use Vendor\usecases\GetProductsFromCart;
+
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class CartController
 {
-    public function handle(
-        ServerRequestInterface $req,
-        ResponseInterface $res,
-        $args = []
-    ) {
+    public function handle(ServerRequestInterface $req)
+    {
         $status = $req->getQueryParams()["status"] ?? 0;
         $template = new RainTpl();
+
+        $getProductsFromCart = new GetProductsFromCart();
+        $products = $getProductsFromCart->execute(
+            @intval($_SESSION["client"][0]["id"]) ?? 0,
+            @intval($_SESSION["provider"][0]["id"]) ?? 0
+        );
+
+        $totalInStorage = [];
+        foreach ($products as $product) {
+
+            $numberFormat = (str_contains($product["total"], "."))
+                ? str_replace(".", "", $product["total"])
+                : $product["total"];
+
+            $removeCommaFromPrice = (str_contains($numberFormat, ","))
+                ? substr($numberFormat, 0, strlen($numberFormat) - 3)
+                : $numberFormat;
+
+            array_push($totalInStorage, (int)$removeCommaFromPrice);
+        }
+
+        $generalTotal = array_sum($totalInStorage);
 
         return $template->setTpl("cart", [
             "provider" => @$_SESSION["provider"],
             "client" => @$_SESSION["client"],
-            "status_code" => intval($status)
+            "status_code" => intval($status),
+            "products" => $products,
+            "total" => $generalTotal
         ]);
     }
 
