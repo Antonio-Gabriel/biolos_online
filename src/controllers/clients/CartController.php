2eb8b6223d919fc8577650dfb81449b0d0b6ad7a
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-01-20 02:17:16 +0100

    add product into cart function

--- src/controllers/clients/CartController.php
+++ src/controllers/clients/CartController.php
@@ -3,6 +3,10 @@
 namespace Vendor\controllers\clients;
 
 use Vendor\config\RainTpl;
+
+use Vendor\validators\Middleware;
+use Vendor\usecases\AddProductIntoCart;
+
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -13,11 +17,49 @@ class CartController
         ResponseInterface $res,
         $args = []
     ) {
+        $status = $req->getQueryParams()["status"] ?? 0;
         $template = new RainTpl();
 
         return $template->setTpl("cart", [
             "provider" => @$_SESSION["provider"],
             "client" => @$_SESSION["client"],
+            "status_code" => intval($status)
         ]);
+    }
+
+    public function addToCart(
+        ServerRequestInterface $req,
+        ResponseInterface $res,
+        $args = []
+    ) {
+        Middleware::hasAuthenticated();
+
+        try {
+
+            $addProductIntoCart = new AddProductIntoCart();
+            $response = $addProductIntoCart->execute(
+                intval($args["product"]),
+                intval(@$_SESSION["client"][0]["id"] ?? 0),
+                intval(@$_SESSION["provider"][0]["id"] ?? 0)
+            );
+
+            if ($response) {
+
+                header("Location: /bioloOnline/cart?status=200");
+                exit();
+            }
+
+            header("Location: /bioloOnline/cart?status=400");
+            exit();
+        } catch (\Exception $th) {
+            echo $th;
+        }
+    }
+
+    public function removeProductToCart(
+        ServerRequestInterface $req,
+        ResponseInterface $res,
+        $args = []
+    ) {
     }
 }
